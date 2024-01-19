43a9092024e72901a7839a0d937d59704776cf0c
stijn <stijn_hanss13@live.com>
2024-01-19 12:30:12 +0100

    store float in double

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        float x = 1f;
+        System.out.println(x);
+        double y = x;
+        System.out.println(y);
+            }
 }
