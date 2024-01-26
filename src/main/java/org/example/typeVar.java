4a49ca866facc3d14122463044ff553b8ff05882
stijn <stijn_hanss13@live.com>
2024-01-26 10:06:57 +0100

    typeVar

---
+++ src/main/java/org/example/typeVar.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class typeVar {
+    // var blabla = "does noet work here";
+    public void whatType() {
+        var name = "x";
+        var size = 7;
+        // size = "cant change to string";
+        var silly
+                = 1;
+       // var question;
+       // question = "wrong, var has to be initialized on the same line;
+
+        // int a, var b = 3
+        // does not work since initializing on the same line has to be of the same type
+
+        // var n = null;
+        // not possible
+    }
+
+/*    public int addition(var a, var b) {
+        return a + b
+    }*/
+    // var cannot be used as parameter variables
+}
