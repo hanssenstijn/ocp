073ccf5a3412455cdd4d8876430a5469f3316290
stijn <stijn_hanss13@live.com>
2024-02-02 13:58:01 +0100

    make text blocks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println(x);
         double y = x;
         System.out.println(y);
+        // A decimal numbers assigned to float needs to have a f;
+        // float w = 1.1;
             }
 }
