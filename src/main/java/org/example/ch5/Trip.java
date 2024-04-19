96146696916f6cfea712cbc6adaaf1a79a71db60
stijn <stijn_hanss13@live.com>
2024-04-19 11:01:01 +0200

    instance and local variable

--- src/main/java/org/example/ch5/Trip.java
+++ src/main/java/org/example/ch5/Trip.java
@@ -2,10 +2,20 @@ package org.example.ch5;
 
 public class Trip {
     public static void main(String[] args) {
+    // instance variable
+        int hunger = 4;
 
+        // dont need to be assigned value, but before used it has to be
+        final int rest;
+        rest = 5;
 
     }
-    public void visitZoo(String name, int waitTime) {}
+    public void visitZoo(String name, int waitTime) {
+        // local variable
+        int snack = 10;
+        // final only modifier for local variables
+        final int snack2 = 10;
+    }
     // does not compile since signature is the same, even-though the names are different
     // public void visitZoo(String name2, int waitTime2) {}
     // change order does compile
