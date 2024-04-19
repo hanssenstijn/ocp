76ba610f26c358a7f19eca819492dca56879fd78
stijn <stijn_hanss13@live.com>
2024-04-19 10:49:25 +0200

    method signature

---
+++ src/main/java/org/example/ch5/Trip.java
@@ -0,0 +1,15 @@
+package org.example.ch5;
+
+public class Trip {
+    public static void main(String[] args) {
+
+
+    }
+    public void visitZoo(String name, int waitTime) {}
+    // does not compile since signature is the same, even-though the names are different
+    // public void visitZoo(String name2, int waitTime2) {}
+    // change order does compile
+    public void visitZoo(int name2, String waitTime2) {}
+
+
+}
