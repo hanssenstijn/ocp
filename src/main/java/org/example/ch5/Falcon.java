7caf563815810d7df03a7106f36604d2bd3f7348
stijn <stijn_hanss13@live.com>
2024-05-03 11:22:40 +0200

    method overloading

---
+++ src/main/java/org/example/ch5/Falcon.java
@@ -0,0 +1,17 @@
+package org.example.ch5;
+
+public class Falcon {
+    public static void main(String[] args) {
+
+    }
+
+    public void fly(int numMiles) {}
+    public void fly(short numFeet) {}
+    public boolean fly() {return false;}
+    void fly(int numMiles, short numFeet) {}
+    public void fly(short numFeet,int numMiles) throws Exception {}
+
+
+
+
+}
