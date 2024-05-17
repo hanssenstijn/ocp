452157899cdf4b799a4bd6f7bbe8488773328885
stijn <stijn_hanss13@live.com>
2024-05-17 10:22:33 +0200

    initialize var

---
+++ src/main/java/org/example/ch6/MouseHouse.java
@@ -0,0 +1,15 @@
+package org.example.ch6;
+
+public class MouseHouse {
+    private final int volume;
+    private final String name = "The Mouse House";
+    private final String name2;
+
+    {
+        volume = 10;
+    }
+
+    public MouseHouse() {
+        this.name2 = "x";
+    }
+}
