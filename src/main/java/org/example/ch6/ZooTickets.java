f172bfda99b276630598dd50a645d8f3baac3dd0
stijn <stijn_hanss13@live.com>
2024-05-17 10:25:10 +0200

    initialize static and instance

---
+++ src/main/java/org/example/ch6/ZooTickets.java
@@ -0,0 +1,24 @@
+package org.example.ch6;
+
+public class ZooTickets {
+    private String name = "BestZoo";
+    {
+        System.out.println(name + "-");
+    }
+    private static int COUNT = 0;
+    static {
+        System.out.println(COUNT + "-");
+    }
+    static {
+        COUNT += 10;
+        System.out.println(COUNT + "-");
+    }
+
+    public ZooTickets() {
+        System.out.println("z-");
+    }
+
+    public static void main(String[] args) {
+        new ZooTickets();
+    }
+}
