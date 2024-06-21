8d116a443ba3172d5f00ac9d50870fa94de9396f
stijn <stijn_hanss13@live.com>
2024-06-21 08:36:58 +0200

    static class

---
+++ src/main/java/org/example/ch7/Park.java
@@ -0,0 +1,12 @@
+package org.example.ch7;
+
+public class Park {
+    static class Ride {
+        private int price = 6;
+    }
+
+    public static void main(String[] args) {
+        var ride = new Ride();
+        System.out.println(ride.price);
+    }
+}
