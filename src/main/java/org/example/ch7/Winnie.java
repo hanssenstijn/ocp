02a9241bb92f14e6b8bdafd7f764909396f6606e
stijn <stijn_hanss13@live.com>
2024-06-21 12:26:47 +0200

    enum abstract methods

---
+++ src/main/java/org/example/ch7/Winnie.java
@@ -0,0 +1,9 @@
+package org.example.ch7;
+
+public class Winnie {
+    // abstract method, each enum has to implement it, therefor the code wont compile
+/*    enum FOOD {
+        BERRIES, INSECTS { public boolean isHealthy() {return true;}}, FISH, ROOTS, COOKIES, HONEY;
+        public abstract boolean isHealthy();
+    }*/
+}
