00f21029ba44394f79a8aac9938d237f4afe937e
stijn <stijn_hanss13@live.com>
2024-07-12 15:39:58 +0200

    Exceptions

--- src/main/java/org/example/ch11/CheckedExceptions.java
+++ src/main/java/org/example/ch11/CheckedExceptions.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 public class CheckedExceptions {
 
     public static void main(String[] args) throws Exception {
+        new CheckedExceptions().fall2(11);
         throw new Exception("bla bla");
         // does not compile since code is unreachable, program stops after previous throw
         // throw new Exception("bla bla");
     }
+
     void fall(int distance) throws IOException {
         if (distance > 10) {
             throw new IOException();
@@ -17,13 +19,23 @@ public class CheckedExceptions {
 
     void fall2(int distance) {
         try {
-            if(distance > 10) {
+            if (distance > 10) {
                 throw new IOException();
             }
-        } catch (Exception e) {
+        } catch (IOException | NumberFormatException e) {
+            e.getMessage();
+        } catch
+        (Exception e) {
             System.out.println(e + "\n");
             System.out.println(e.getMessage() + "\n");
             e.printStackTrace();
+        } finally {
+            System.out.println("Finally");
         }
+    }
+
+    public void setNumberEggs(int numberEggs) {
+        if (numberEggs < 0)
+            throw new IllegalArgumentException("# eggs must not be negative");
     }
 }
