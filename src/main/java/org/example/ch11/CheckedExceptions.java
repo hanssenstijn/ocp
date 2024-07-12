14de98751a87504e61681009dd78dab8ac377ea4
stijn <stijn_hanss13@live.com>
2024-07-12 13:15:53 +0200

    Exceptions

--- src/main/java/org/example/ch11/CheckedExceptions.java
+++ src/main/java/org/example/ch11/CheckedExceptions.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 
 public class CheckedExceptions {
 
+    public static void main(String[] args) throws Exception {
+        throw new Exception("bla bla");
+        // does not compile since code is unreachable, program stops after previous throw
+        // throw new Exception("bla bla");
+    }
     void fall(int distance) throws IOException {
         if (distance > 10) {
             throw new IOException();
