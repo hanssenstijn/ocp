4580572ec5066d01be912c8e0e5a1db676e43e62
stijn <stijn_hanss13@live.com>
2024-08-30 10:34:54 +0200

    Threads

---
+++ src/main/java/org/example/ch13/StreamOf.java
@@ -0,0 +1,25 @@
+package org.example.ch13;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamOf {
+    public static void main(String[] args) {
+        var cats = Stream.of("leopeard","lynx","ocelot","puma").parallel();
+        var bears = Stream.of("panda","grizzly","polar").parallel();
+
+        var data = Stream.of(cats,bears).flatMap(s -> s).collect(Collectors.groupingByConcurrent(s -> !s.startsWith(
+                "p")));
+        System.out.println(data.get(false).size() + " " + data.get(true).size());
+
+        final long[] value2 = {0};
+        IntStream.iterate(1,i -> 1)
+                .limit(100)
+                .parallel()
+                .forEach(i -> ++value2[0]);
+        System.out.println(value2[0]);
+
+
+    }
+}
