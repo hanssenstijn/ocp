07889d18ac8113c64fa26cb6176581af8fe2dda5
stijn <stijn_hanss13@live.com>
2024-10-04 10:19:38 +0200

    CallStatement

--- src/main/java/org/example/ch15/CallStatement.java
+++ src/main/java/org/example/ch15/CallStatement.java
@@ -3,14 +3,25 @@ package org.example.ch15;
 import java.sql.*;
 
 public class CallStatement {
-    public static void main(String[] args) throws SQLException {
-    }
 
     void withoutParameters() throws SQLException {
         String url = "url";
         String sql = "{call read_e_names()}";
         Connection conn = DriverManager.getConnection(url);
         try (CallableStatement cs = conn.prepareCall(sql); ResultSet rs = cs.executeQuery()) {
+            while (rs.next()) {
+                System.out.println(rs.getString(3));
+            }
+        }
+    }
+
+    void passingIN() throws SQLException {
+        String url = "url";
+        String sql = "{call read_names_by_letter(?)}";
+        Connection conn = DriverManager.getConnection(url);
+        try (CallableStatement cs = conn.prepareCall(sql,ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_READ_ONLY); ResultSet rs =
+                cs.executeQuery()) {
+            cs.setString("prefix","Z");
             while (rs.next()) {
                 System.out.println(rs.getString(3));
             }
